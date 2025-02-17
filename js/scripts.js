b757d08ccdef8eb885b53aaedf142a291d9aaaca
jarment32 <josemartin250389@gmail.com>
2025-02-17 16:53:35 +0100

    Initial commit with project structure

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    showContent('add-day');
+});
+
+function showContent(id) {
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => {
+        content.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
